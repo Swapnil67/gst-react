c2b6e158dcbe5847bffe094f77853e13f1ec7863
Swapnil67 <swapniladsul017@gmail.com>
2021-08-20 14:53:34 +0530

    enabling cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule, { cors: true });
+//   await app.listen(3000);
+// }
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({  // wrong!  in my case, anyway
+    origin: 'http://localhost:3003',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
+  await app.listen(process.env.PORT || 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
